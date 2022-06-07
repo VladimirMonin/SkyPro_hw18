dec9a28b5e4b4e060dd327c8a9383799c495bb22
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-06-07 20:06:34 +0600

    fix: get_one_raw для того, чтобы оставить сериализацию в DAO

--- app/dao/movies.py
+++ app/dao/movies.py
@@ -13,12 +13,25 @@ class MoviesDAO:
         return result, 200
 
     def get_one(self, mid):
-        movie = Movies.query.one(mid)
+        movie = Movies.query.get(mid)
         result = MoviesSchema().dump(movie)
         return result, 200
+
+    def get_one_raw(self, mid):
+        movie = Movies.query.get(mid)
+        return movie
 
     def create(self, data):
         movie = Movies(**data)
         self.session.add(movie)
         self.session.commit()
         self.session.close()
+
+    def update(self, data):
+        self.session.add(data)
+        self.session.commit()
+
+    def delete(self, mid):
+        data = self.get_one(mid)
+        self.session.delete(data)
+        self.session.commit()
--- app/views/movies.py
+++ app/views/movies.py
@@ -1,5 +1,5 @@
 from flask_restx import Resource, Namespace
-from flask import request
+from flask import request, jsonify
 from app.container import movies_service
 
 movies_ns = Namespace('movies')
@@ -12,10 +12,28 @@ class Movies(Resource):
 
     def post(self):
         data = request.get_json()
-        movies_service.create(data)
-        return '', 201
+        movies_id = data['id']  # Получаем ID из запроса (которого там может и не быть, т.к. автоинкремент работает!)
+        movies_service.create(data)   # Создаем новую запись в БД
+        #TODO Сделать по красоте - чтобы POST Работал без передачи IP с запросом
+        response = jsonify()  # Делаем пункт из ДЗ \\ Заголовок Location есть в POST на создание сущности.
+        response.status_code = 201  # Код ответа
+        response.headers['location'] = f'movies/{movies_id}'  # Адрес для "перехода" после добавления
+        return response
 
-@movies_ns.route('<int:mid>')
+@movies_ns.route('/<int:mid>/')
 class Movies(Resource):
-    def get(self, mid) -> [dict]:
+    def get(self, mid: int) -> [dict]:
         return movies_service.get_one(mid), 200
+
+    def put(self, mid: int):
+        data = request.json
+        data['id'] = mid
+        movies_service.update(data)
+        return '', 204
+
+    def delete(self, mid: int):
+        movies_service.delete(mid)
+        return '', 204
+
+
+
